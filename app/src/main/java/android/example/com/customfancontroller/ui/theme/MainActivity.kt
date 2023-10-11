6a2298f725fa56c13de82541422d38f8216ed136
hsu-hsu <hsuyethin@gmail.com>
2023-10-11 23:43:36 +0700

    add custom view named dialview

---
+++ app/src/main/java/android/example/com/customfancontroller/ui/theme/MainActivity.kt
@@ -0,0 +1,12 @@
+package android.example.com.customfancontroller.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.example.com.customfancontroller.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
